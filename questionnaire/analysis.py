c379e751d5eeed8752a12791882eb345c5c434d1
tbohne <tbohne@uni-osnabrueck.de>
2021-11-13 13:08:20 +0100

    refine plot vis

--- questionnaire/analysis.py
+++ questionnaire/analysis.py
@@ -59,17 +59,21 @@ def plot(lta_problems):
         difficulty_votes.append(problem.get_difficulty())
         likelihood_votes.append(problem.get_likelihood())
 
-    ax.scatter(impact_votes, difficulty_votes, likelihood_votes, marker='D', color='red')
+    s = [75 for n in range(len(names))]
+
+    ax.scatter(impact_votes, difficulty_votes, likelihood_votes, marker='D', color='red', s=s)
+    ax.scatter(1, 1, 1, marker='X', color='gold', s=[75])
 
     print(names)
 
     for i, problem in enumerate(names):
-        ax.text(impact_votes[i], difficulty_votes[i], likelihood_votes[i], problem)
+        ax.text(impact_votes[i], difficulty_votes[i], likelihood_votes[i], problem, fontsize=15)
+    ax.text(1, 1, 1, 'optimum', fontsize=15)
 
-    ax.set_xlabel('impact (1: high, 2: medium, 3: low)')
-    ax.set_ylabel('difficulty (1: easy, 2: medium, 3: hard)')
-    ax.set_zlabel('likelihood (1: very likely, 2: occurs, 3: highly unlikely')
-
+    ax.set_xlabel('impact (1: high, 2: medium, 3: low)', fontsize=15)
+    ax.set_ylabel('difficulty (1: easy, 2: medium, 3: hard)', fontsize=15)
+    ax.set_zlabel('likelihood (1: very likely, 2: occurs, 3: highly unlikely', fontsize=15)
+    
     plt.show()
     
 
