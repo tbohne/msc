4d26a7eb67eee5821f444c11bb0b88ce9f37015b
tbohne <tbohne@uni-osnabrueck.de>
2022-03-08 14:02:25 +0100

    refactor plot gen

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,42 +1,21 @@
 library(ggplot2)
 
+gen_plot <- function(plotPointsPre, x_name, y_name, filename) {
+    finalPlot <- plotPointsPre + geom_point() + xlab(x_name) + ylab(y_name) + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
+    ggsave(finalPlot, file = filename, width=6, height=4)
+}
+
 input <- read.csv(file = "placeholder.csv", header = TRUE, sep = ",")
 
-plotPointsPre <- ggplot(data = input, aes(x = duration, y = experiment, color = completed, group = completed))
-finalPlot <- plotPointsPre + geom_point() + xlab("duration (h)") + ylab("experiment")  + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
-ggsave(finalPlot, file = "duration.png", width=6, height=4)
-
-plotPointsPre <- ggplot(data = input, aes(x = correct_contingencies, y = experiment, color = completed, group = completed))
-finalPlot <- plotPointsPre + geom_point() + xlab("correct_contingencies") + ylab("experiment") + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
-ggsave(finalPlot, file = "corr_cont.png", width=6, height=4)
-
-plotPointsPre <- ggplot(data = input, aes(x = false_positives, y = experiment, color = completed, group = completed))
-finalPlot <- plotPointsPre + geom_point() + xlab("false positive contingencies") + ylab("experiment") + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
-ggsave(finalPlot, file = "false_positives.png", width=6, height=4)
-
-plotPointsPre <- ggplot(data = input, aes(x = false_negatives, y = experiment, color = completed, group = completed))
-finalPlot <- plotPointsPre + geom_point() + xlab("false negative contingencies") + ylab("experiment") + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
-ggsave(finalPlot, file = "false_negatives.png", width=6, height=4)
-
-plotPointsPre <- ggplot(data = input, aes(x = correct_no_contingency, y = experiment, color = completed, group = completed))
-finalPlot <- plotPointsPre + geom_point() + xlab("correct NO contingencies") + ylab("experiment") + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
-ggsave(finalPlot, file = "correct_no_contingencies.png", width=6, height=4)
-
-plotPointsPre <- ggplot(data = input, aes(x = unexpected_contingencies, y = experiment, color = completed, group = completed))
-finalPlot <- plotPointsPre + geom_point() + xlab("unexpected contingencies") + ylab("experiment") + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
-ggsave(finalPlot, file = "unexpected_contingencies.png", width=6, height=4)
-
-plotPointsPre <- ggplot(data = input, aes(x = completed_tasks, y = experiment, color = completed, group = completed))
-finalPlot <- plotPointsPre + geom_point() + xlab("completed tasks") + ylab("experiment") + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
-ggsave(finalPlot, file = "completed_tasks.png", width=6, height=4)
-
-plotPointsPre <- ggplot(data = input, aes(x = charge_cycles, y = experiment, color = completed, group = completed))
-finalPlot <- plotPointsPre + geom_point() + xlab("charge cycles") + ylab("experiment") + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
-ggsave(finalPlot, file = "charge_cycles.png", width=6, height=4)
-
-plotPointsPre <- ggplot(data = input, aes(x = (correct_contingencies + correct_no_contingency) / (correct_contingencies + correct_no_contingency + false_positives + false_negatives + unexpected_contingencies) * 100, y = experiment, color = completed, group = completed))
-finalPlot <- plotPointsPre + geom_point() + xlab("expected response to fail sim (%)") + ylab("experiment") + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
-ggsave(finalPlot, file = "expected_res.png", width=6, height=4)
+gen_plot(ggplot(data = input, aes(x = duration, y = experiment, color = completed, group = completed)), "duration (h)", "experiment", "duration.png")
+gen_plot(ggplot(data = input, aes(x = correct_contingencies, y = experiment, color = completed, group = completed)), "correct_contingencies", "experiment", "corr_cont.png")
+gen_plot(ggplot(data = input, aes(x = false_positives, y = experiment, color = completed, group = completed)), "false positive contingencies", "experiment", "false_positives.png")
+gen_plot(ggplot(data = input, aes(x = false_negatives, y = experiment, color = completed, group = completed)), "false negative contingencies", "experiment", "false_negatives.png")
+gen_plot(ggplot(data = input, aes(x = correct_no_contingency, y = experiment, color = completed, group = completed)), "correct NO contingencies", "experiment", "correct_no_contingencies.png")
+gen_plot(ggplot(data = input, aes(x = unexpected_contingencies, y = experiment, color = completed, group = completed)), "unexpected contingencies", "experiment", "unexpected_contingencies.png")
+gen_plot(ggplot(data = input, aes(x = completed_tasks, y = experiment, color = completed, group = completed)), "completed tasks", "experiment", "completed_tasks.png")
+gen_plot(ggplot(data = input, aes(x = charge_cycles, y = experiment, color = completed, group = completed)), "charge cycles", "experiment", "charge_cycles.png")
+gen_plot(ggplot(data = input, aes(x = (correct_contingencies + correct_no_contingency) / (correct_contingencies + correct_no_contingency + false_positives + false_negatives + unexpected_contingencies) * 100, y = experiment, color = completed, group = completed)), "expected response to fail sim (%)", "experiment", "expected_res.png")
 
 compute_avg_duration <- function() {
     costs <- subset(input, select = c(duration))
