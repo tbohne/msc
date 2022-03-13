49058065d0428e7f2da04da679607ba9d76a9799
tbohne <tbohne@uni-osnabrueck.de>
2022-03-13 13:55:28 +0100

    impl mode times script

---
+++ analysis/mode_times.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+
+gen_plot <- function(plotPointsPre, y_name, x_name, filename) {
+    finalPlot <- plotPointsPre + coord_flip() + xlab(x_name) + ylab(y_name)
+    ggsave(finalPlot, file = filename, width=6, height=4)
+}
+
+input <- read.csv(file = "modes.csv", header = TRUE, sep = ",")
+
+ggp <- ggplot(input, aes(x = experiment, y = mode_time, fill = mode)) + geom_bar(position = "stack", stat = "identity")
+
+gen_plot(ggp, "mode times", "experiment", "mode_times.png")
