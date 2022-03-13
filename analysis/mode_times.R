3f17e54b480be0b103f36264ae0f69ef994a6cd4
tbohne <tbohne@uni-osnabrueck.de>
2022-03-13 18:29:30 +0100

    adjust colors

--- analysis/mode_times.R
+++ analysis/mode_times.R
@@ -7,6 +7,6 @@ gen_plot <- function(plotPointsPre, y_name, x_name, filename) {
 
 input <- read.csv(file = "modes.csv", header = TRUE, sep = ",")
 
-ggp <- ggplot(input, aes(x = experiment, y = mode_time, fill = mode)) + geom_bar(position = "stack", stat = "identity")
+ggp <- ggplot(input, aes(x = experiment, y = mode_time, fill = mode)) + geom_bar(position = "stack", stat = "identity") + scale_fill_manual("mode", values = c("cata" = "#003f5c", "charge" = "#2f4b7c", "cont" = "#665191", "dock" = "#a05195", "scan" = "#d45087", "traverse" = "#f95d6a", "undock" = "#ff7c43", "wait" = "#ffa600"))
 
 gen_plot(ggp, "mode times (s)", "experiment", "mode_times.png")
